1f758aec97175b9377f1ec221d7aaf73f068ed56
francisco <frmauro8@hotmail.com>
2019-09-03 20:53:56 -0300

    create dockerfile

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://adm:Mau123!@#@cluster0-shard-00-00-ixmzt.mongodb.net:27017,cluster0-shard-00-01-ixmzt.mongodb.net:27017,cluster0-shard-00-02-ixmzt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
-mongoose.connect('mongodb://127.0.0.1/autenticationcle');
+//mongoose.connect('mongodb://127.0.0.1/autenticationcle');
+mongoose.connect('mongodb://192.168.99.1/autenticationcle');
+
